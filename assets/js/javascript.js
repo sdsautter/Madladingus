4c0b42b19d5bcca7e9aaf317a6de312fb1ee46cc
sdsautter <sdsautter@gmail.com>
2017-02-26 15:11:25 -0500

    Added a little more to the index p

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,7 +3,7 @@
 function welcome() {
 	var name = prompt("What is your name?")
 	document.getElementById("welcome").innerHTML = 
-		"<p>Hello " + name + ",</p> <p>Welcome to Madladingus, a wonderful world of maddening and ad-liberating storytelling told through the wondrous paradigm of none-other-than yourself!</p><p>I started this website because... well... to be honest, I was bored, and I wanted another project to do that I might be able to involve my wife with because she's a good writer. I've always enjoyed Mad Libs&reg; so I thought, 'Why not try creating my own? How hard could it be?' Turns out I've done easier things and this project wasn't as exciting for my wife as it was for me.";
+		"<p>Hello " + name + ",</p> <p>Welcome to Madladingus, a wonderful world of maddening and ad-liberating storytelling told through the wondrous paradigm of none-other-than yourself!</p><p>I started this website because... well... to be honest, I was bored, and I wanted another project to do that I might be able to involve my wife with because she's a good writer. I've always enjoyed Mad Libs&reg; so I thought, 'Why not try creating my own? How hard could it be?' Turns out I've done easier things, and this project wasn't as exciting or enticing of a prospect for my wife as I'd hope it to be.";
 }
 
 window.onload = function(){
